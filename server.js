60fafab67bd46afbd465c1d701816548eef2f3c7
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-06-19 07:57:00 +0200

    ongoing

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-// process.env.PORT ||
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+// process.env.PORT ||
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
